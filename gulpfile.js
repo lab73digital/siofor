4f5f10a21bf6ffa2392bbd34c717110da3d50989
chesovnik <kitkatne@gmail.com>
2017-09-22 18:10:13 +0300

    Added variables and tuned the structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,88 +1,90 @@
 var gulp           = require('gulp'),
-		gutil          = require('gulp-util' ),
-		sass           = require('gulp-sass'),
-		browserSync    = require('browser-sync'),
-		concat         = require('gulp-concat'),
-		uglify         = require('gulp-uglify'),
-		cleanCSS       = require('gulp-clean-css'),
-		rename         = require('gulp-rename'),
-		del            = require('del'),
-		imagemin       = require('gulp-imagemin'),
-		cache          = require('gulp-cache'),
-		autoprefixer   = require('gulp-autoprefixer'),
-		bourbon        = require('node-bourbon'),
-		ftp            = require('vinyl-ftp'),
-		notify         = require("gulp-notify");
+    gutil          = require('gulp-util' ),
+    sass           = require('gulp-sass'),
+    browserSync    = require('browser-sync'),
+    concat         = require('gulp-concat'),
+    uglify         = require('gulp-uglify'),
+    cleanCSS       = require('gulp-clean-css'),
+    rename         = require('gulp-rename'),
+    del            = require('del'),
+    imagemin       = require('gulp-imagemin'),
+    cache          = require('gulp-cache'),
+    autoprefixer   = require('gulp-autoprefixer'),
+    bourbon        = require('node-bourbon'),
+    ftp            = require('vinyl-ftp'),
+    notify         = require("gulp-notify");
 
-var productname = 'siofor';
+
 
 // Скрипты проекта
 gulp.task('scripts', function() {
-	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
-		'app/js/common.js' // Всегда в конце
-		])
-	.pipe(concat('scripts.min.js'))
-	.pipe(uglify())
-	.pipe(gulp.dest('app/js'))
-	.pipe(browserSync.reload({stream: true}));
+    return gulp.src([
+        'app/libs/jquery/dist/jquery.min.js',
+        'app/js/common.js' // Всегда в конце
+    ])
+        .pipe(concat('scripts.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('app/js'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('browser-sync', function() {
-	browserSync({
-		server: {
-			baseDir: 'app'
-		},
-		notify: false
-	});
+    browserSync({
+        server: {
+            baseDir: 'app'
+        },
+        notify: false
+    });
 });
 
 gulp.task('sass', function() {
-	return gulp.src('app/sass/**/*.sass')
-	.pipe(sass({
-		includePaths: bourbon.includePaths
-	}).on("error", notify.onError()))
-	.pipe(rename({suffix: '.min', prefix : ''}))
-	.pipe(autoprefixer(['last 15 versions']))
-	.pipe(cleanCSS())
-	.pipe(gulp.dest('app/css'))
-	.pipe(browserSync.reload({stream: true}));
+    return gulp.src('app/sass/**/*.sass')
+        .pipe(sass({
+            includePaths: bourbon.includePaths
+        }).on("error", notify.onError()))
+        .pipe(rename({suffix: '.min', prefix : ''}))
+        .pipe(autoprefixer(['last 15 versions']))
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('app/css'))
+        .pipe(browserSync.reload({stream: true}));
 });
 
 gulp.task('watch', ['sass', 'scripts', 'browser-sync'], function() {
-	gulp.watch('app/sass/**/*.sass', ['sass']);
-	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['scripts']);
-	gulp.watch('app/*.html', browserSync.reload);
+    gulp.watch('app/sass/**/*.sass', ['sass']);
+    gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['scripts']);
+    gulp.watch('app/*.html', browserSync.reload);
 });
 
 gulp.task('imagemin', function() {
-	return gulp.src('app/img/**/*')
-	.pipe(cache(imagemin()))
-	.pipe(gulp.dest('dist/img'));
+    return gulp.src('app/img/**/*')
+        .pipe(cache(imagemin()))
+        .pipe(gulp.dest('dist/img'));
 });
 
 gulp.task('build', ['removedist', 'imagemin', 'sass', 'scripts'], function() {
 
-	var buildFiles = gulp.src([
-		'app/*.html',
-		'app/.htaccess'
-		]).pipe(gulp.dest('dist'));
+    var buildFiles = gulp.src([
+        'app/*.html',
+        'app/.htaccess'
+    ]).pipe(gulp.dest('dist'));
 
-	var buildCss = gulp.src([
-		'app/css/main.min.css',
-		]).pipe(gulp.dest('dist/css'));
+    var buildCss = gulp.src([
+        'app/css/main.min.css',
+    ]).pipe(gulp.dest('dist/css'));
 
-	var buildJs = gulp.src([
-		'app/js/scripts.min.js'
-		]).pipe(gulp.dest('dist/js'));
+    var buildJs = gulp.src([
+        'app/js/scripts.min.js'
+    ]).pipe(gulp.dest('dist/js'));
 
-	var buildFonts = gulp.src([
-		'app/fonts/**/*']
-		).pipe(gulp.dest('dist/fonts'));
+    var buildFonts = gulp.src([
+        'app/fonts/**/*']
+    ).pipe(gulp.dest('dist/fonts'));
 
 });
 
 // Build for likar.info
+
+var productname = 'siofor';
 
 gulp.task('build-likar', ['removelikar-info', 'sass', 'scripts'], function() {
 
@@ -90,7 +92,7 @@ gulp.task('build-likar', ['removelikar-info', 'sass', 'scripts'], function() {
         .pipe(cache(imagemin()))
         .pipe(gulp.dest('likar-info/special/' + productname + '/img'));
 
-	var buildFiles = gulp.src([
+    var buildFiles = gulp.src([
         'app/*.html',
         'app/.htaccess'
     ]).pipe(gulp.dest('likar-info/special/' + productname));
@@ -113,20 +115,20 @@ gulp.task('build-likar', ['removelikar-info', 'sass', 'scripts'], function() {
 
 gulp.task('deploy', function() {
 
-	var conn = ftp.create({
-		host:      'hostname.com',
-		user:      'username',
-		password:  'userpassword',
-		parallel:  10,
-		log: gutil.log
-	});
+    var conn = ftp.create({
+        host:      'hostname.com',
+        user:      'username',
+        password:  'userpassword',
+        parallel:  10,
+        log: gutil.log
+    });
 
-	var globs = [
-	'dist/**',
-	'dist/.htaccess',
-	];
-	return gulp.src(globs, {buffer: false})
-	.pipe(conn.dest('/path/to/folder/on/server'));
+    var globs = [
+        'dist/**',
+        'dist/.htaccess',
+    ];
+    return gulp.src(globs, {buffer: false})
+        .pipe(conn.dest('/path/to/folder/on/server'));
 
 });
 
