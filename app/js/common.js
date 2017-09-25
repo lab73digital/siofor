72689c3c72fbc2693dc9a455377aba6b9a32c7ee
chesovnik <kitkatne@gmail.com>
2017-09-25 16:51:36 +0300

    Made pack switcher

--- app/js/common.js
+++ app/js/common.js
@@ -1,44 +1,88 @@
+var counter = 850;
+
 $(document).ready(function () {
     $( "#button-left" ).on('click', function() {
-        $( "#biton-block_500" ).slideToggle();
-        //$('.siofor-instruction__second-block_biton').toggleClass('siofor-instruction__second-block_biton_1000v');
+        if (counter === 850) {
+            $("#biton-block_500_p").addClass("hovered-item-500");
+            counter = 500;
+            $(".siofor-instruction__second-block_biton_850").hide();
+            $(".siofor-instruction__second-block_biton_1000").hide();
+            $("#biton-block_1000_p").removeClass("hovered-item-1000");
+            $("#biton-block_850_p").removeClass("hovered-item-850");
+            $(".siofor-instruction__second-block_biton_500").show();
+        } else if (counter === 1000) {
+            $("#biton-block_850_p").addClass("hovered-item-850");
+            counter = 850;
+            $(".siofor-instruction__second-block_biton_500").hide();
+            $(".siofor-instruction__second-block_biton_1000").hide();
+            $("#biton-block_500_p").removeClass("hovered-item-500");
+            $("#biton-block_1000_p").removeClass("hovered-item-1000");
+            $(".siofor-instruction__second-block_biton_850").show();
+        } else {
+            $("#biton-block_1000_p").addClass("hovered-item-1000");
+            counter = 1000;
+            $(".siofor-instruction__second-block_biton_500").hide();
+            $(".siofor-instruction__second-block_biton_850").hide();
+            $("#biton-block_500_p").removeClass("hovered-item-500");
+            $("#biton-block_850_p").removeClass("hovered-item-850");
+            $(".siofor-instruction__second-block_biton_1000").show();
+        }
+
     });
+
     $( "#button-right" ).on('click', function() {
-        $(".siofor-instruction__second-block_biton_850").toggle(function () {
-            $(".siofor-instruction__second-block_biton_1000").toggle(function () {
-                $(".siofor-instruction__second-block_biton_500").toggle();
-            })
-        })
+        if (counter === 850) {
+            $("#biton-block_1000_p").addClass("hovered-item-1000");
+            counter = 1000;
+            $(".siofor-instruction__second-block_biton_500").hide();
+            $(".siofor-instruction__second-block_biton_850").hide();
+            $("#biton-block_500_p").removeClass("hovered-item-500");
+            $("#biton-block_850_p").removeClass("hovered-item-850");
+            $(".siofor-instruction__second-block_biton_1000").show();
+        } else if (counter === 1000) {
+            $("#biton-block_500_p").addClass("hovered-item-500");
+            counter = 500;
+            $(".siofor-instruction__second-block_biton_850").hide();
+            $(".siofor-instruction__second-block_biton_1000").hide();
+            $("#biton-block_1000_p").removeClass("hovered-item-1000");
+            $("#biton-block_850_p").removeClass("hovered-item-850");
+            $(".siofor-instruction__second-block_biton_500").show();
+        } else {
+            $("#biton-block_850_p").addClass("hovered-item-850");
+            counter = 850;
+            $(".siofor-instruction__second-block_biton_500").hide();
+            $(".siofor-instruction__second-block_biton_1000").hide();
+            $("#biton-block_500_p").removeClass("hovered-item-500");
+            $("#biton-block_1000_p").removeClass("hovered-item-1000");
+            $(".siofor-instruction__second-block_biton_850").show();
+        }
+
     });
-    // $( "#biton-block_850_p" ).on('click', function() {
-    //     $( "#biton-block_850" ).slideToggle();
-    //     //$("#biton-block_850").toggleClass('.siofor-instruction__second-block_biton_850');
-    // });
-    // $( "#biton-block_500_p" ).on('click', function() {
-    //     $( "#biton-block_500" ).slideToggle();
-    //     //$('.siofor-instruction__second-block_biton').toggleClass('siofor-instruction__second-block_biton_1000v');
-    // });
+
 
     $("#biton-block_500_p").click(function(){
         $("#biton-block_500_p").addClass("hovered-item-500");
-        $(".siofor-instruction__second-block_biton_850").hide("slow");
-            $(".siofor-instruction__second-block_biton_1000").hide("slow");
+        counter = 500;
+        $(".siofor-instruction__second-block_biton_850").hide();
+            $(".siofor-instruction__second-block_biton_1000").hide();
                 $("#biton-block_1000_p").removeClass("hovered-item-1000");
                 $("#biton-block_850_p").removeClass("hovered-item-850");
                 $(".siofor-instruction__second-block_biton_500").show();
     });
     $("#biton-block_1000_p").click(function(){
         $("#biton-block_1000_p").addClass("hovered-item-1000");
-        $(".siofor-instruction__second-block_biton_500").hide("slow");
-            $(".siofor-instruction__second-block_biton_850").hide("slow");
+        counter = 1000;
+        $(".siofor-instruction__second-block_biton_500").hide();
+            $(".siofor-instruction__second-block_biton_850").hide();
                 $("#biton-block_500_p").removeClass("hovered-item-500");
                 $("#biton-block_850_p").removeClass("hovered-item-850");
                 $(".siofor-instruction__second-block_biton_1000").show();
     });
     $("#biton-block_850_p").click(function(){
         $("#biton-block_850_p").addClass("hovered-item-850");
-        $(".siofor-instruction__second-block_biton_500").hide("slow");
-            $(".siofor-instruction__second-block_biton_1000").hide("slow");
+        counter = 850;
+        $(".siofor-instruction__second-block_biton_500").hide();
+            $(".siofor-instruction__second-block_biton_1000").hide();
                 $("#biton-block_500_p").removeClass("hovered-item-500");
                 $("#biton-block_1000_p").removeClass("hovered-item-1000");
                 $(".siofor-instruction__second-block_biton_850").show();
